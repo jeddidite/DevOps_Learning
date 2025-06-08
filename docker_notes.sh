78d6d362331cef9b1442928602fd0565e8c6c4bc
jeddidite <charlesj.persson@gmail.com>
2025-06-08 11:35:03 -0400

    Adding docker networking concepts and commands to docker_notes.sh

--- docker_notes.sh
+++ docker_notes.sh
@@ -30,6 +30,8 @@ docker run -p 8080:80 hello-world-app
 # On any server, anywhere in the world
 docker pull jeddidite/devops_learning_app:latest
 docker run -d -p 80:80 jeddidite/devops_learning_app:latest
+# But if you want to control the name of the container, instead of it generating a random name, you can use the --name flag:
+docker run -d --name devops_container -p 80:80 jeddidite/devops_learning_app:latest
 
 #Stopping a container
 docker ps
@@ -56,3 +58,48 @@ docker run -v db:/app/data nginx # Run an nginx container with the 'db' volume m
 docker run -d -v db:/app/data nginx # Run in detached mode
 docker volume ls # List all volumes
 docker volume inspect db # Inspect the 'db' volume
+
+# Docker Networks:
+#Bridge vs host vs none. 
+#Bridge is the defualt network and it lets local containers talk to each other. 
+#The host network allows local containers to talk to the outside world. 
+#The none network isolates the container from all networks.
+
+docker network ls # List all networks
+docker network create devops_network # Create a new network named 'my-network'  
+docker network inspect devops_network # Inspect the 'my-network' network
+# Running a container on a specific network
+docker run -d --name test_container_1 --network devops_network nginx # Run an nginx container on 'my-network'
+docker run -d --name test_container_2 --network devops_network nginx # Run an nginx container on 'my-network'
+
+# Testing network connectivity between containers
+docker exec -it test_container_1 curl http://test_container_2 # Curl 'test_container_2' from 'test_container_1'
+docker exec -it test_container_2 curl http://test_container_1 # Curl 'test_container_1' from 'test_container_2'
+
+docker exec -it test_container_1 apt-get update    # Update package lists in 'test_container_1'
+docker exec -it test_container_1 apt-get install -y iputils-ping # Install ping in 'test_container_1'
+docker exec -it test_container_2 apt-get update # Update package lists in 'test_container_2'
+docker exec -it test_container_2 apt-get install -y iputils-ping # Install ping in 'test_container_2'
+
+# Testing network connectivity between containers using ping
+docker exec -it test_container_1 ping test_container_2 # Ping 'test_container_2' from 'test_container_1'
+docker exec -it test_container_2 ping test_container_1 # Ping 'test_container_1' from 'test_container_2'
+
+# Testing network connectivity from a container to the host
+docker exec -it test_container_1 ping host.docker.internal # Ping the host from 'test_container_1' (works on Docker Desktop)
+docker exec -it test_container_2 ping host.docker.internal # Ping the host from 'test_container_2' (works on Docker Desktop)
+
+# Testing network connectivity from a container to the internet
+docker exec -it test_container_1 ping google.com # Ping google.com from 'test_container_1'
+docker exec -it test_container_2 ping google.com # Ping google.com from 'test_container_2'
+
+# Testing network connectivity from the host to a container
+docker exec -it devops_container ping test_container_1 # Ping 'test_container_1' from the host
+docker exec -it devops_container ping test_container_2 # Ping 'test_container_2' from the host
+
+# Running a container with a specific network alias 
+docker run -d --name devops_container --network devops_network --network-alias devops_network nginx # Run an nginx container with alias 'my-nginx' on 'my-network'
+# Connecting an existing container to a network
+docker network connect devops_network devops_container # Connect 'my-nginx' to 'my-network'
+# Disconnecting a container from a network
+docker network disconnect devops_network devops_container # Disconnect 'my-nginx' from 'my-network'
