1ae30a6a9159d5d597ffe2083de6a879b41a1dd7
jeddidite <charlesj.persson@gmail.com>
2025-06-08 12:36:26 -0400

    Server setup in wrong directory. Adding it now.

---
+++ server_setup.sh
@@ -0,0 +1,9 @@
+# Git setup
+git clone https://github.com/jeddidite/DevOps_Learning
+cd .\DevOps_Learning\docker-hello-world\
+
+# Creating Docker containers and networks
+docker-compose up -d
+
+# When you are done: 
+docker-compose down
